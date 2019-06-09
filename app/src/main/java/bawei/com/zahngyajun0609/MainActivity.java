071ac7d7fc74c69802843955e4c9aab1d7330f36
jaj <1076035699@qq.com>
2019-06-09 20:16:51 +0800

    第三次改动           2019年6月9日20:16:42

--- app/src/main/java/bawei/com/zahngyajun0609/MainActivity.java
+++ app/src/main/java/bawei/com/zahngyajun0609/MainActivity.java
@@ -4,13 +4,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 private Button but;
+private TextView text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         but  = findViewById(R.id.but);
+        text= findViewById(R.id.text);
     }
 }
